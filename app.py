d140c3d4ed5fc1badfa0ec89dfc0fe4d16d260b8
Abhijeet pandey <itsmeabhijeetpandey@gmail.com>
2017-05-26 09:49:46 +0530

    Update app.py
    string to int

--- app.py
+++ app.py
@@ -34,9 +34,9 @@ def makeWebhookResult(req):
     parameters = result.get("parameters")
     zone = parameters.get("geo-country")
 
-    cost = {'Europe':100, 'India':delhi, 'Pakistan':islamabad, 'Asia':400, 'Africa':500}
+    cost = {'Europe':100, 'India':delhi, 'Pakistan':300, 'Asia':400, 'Africa':500}
 
-    speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
+    speech = "The cost of shipping to " + zone + " is " + cost[zone] + " euros."
 
     print("Response:")
     print(speech)
